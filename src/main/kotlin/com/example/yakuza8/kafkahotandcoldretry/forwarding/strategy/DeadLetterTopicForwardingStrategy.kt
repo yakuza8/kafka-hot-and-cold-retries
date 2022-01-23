9aa8d1380fd4b136594ad5c75718fc44be678a86
Berat Cankar <berat.cankar@udemy.com>
2022-01-23 11:23:13 +0300

    Define dead letter topic forwarding strategy

--- src/main/kotlin/com/example/yakuza8/kafkahotandcoldretry/forwarding/strategy/DeadLetterTopicForwardingStrategy.kt
+++ src/main/kotlin/com/example/yakuza8/kafkahotandcoldretry/forwarding/strategy/DeadLetterTopicForwardingStrategy.kt
@@ -1,4 +1,9 @@
 package com.example.yakuza8.kafkahotandcoldretry.forwarding.strategy
 
-class DeadLetterTopicForwardingStrategy {
+class DeadLetterTopicForwardingStrategy(
+    val deadLetterTopicName: String
+) : TopicForwardingStrategy {
+    override fun decideDestinationTopic(): String {
+        return deadLetterTopicName
+    }
 }
